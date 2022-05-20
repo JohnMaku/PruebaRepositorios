0592a6453ebba069948d8bd61b8dd8df08d07aa7
john sanchez <fsanchezvanegas@gmail.com>
2022-05-20 14:11:37 -0500

    nueva prueba salvando

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-//programa para probar funcionamiento del sensor temp humd
+//programa para probar funcionamiento del sensor temp humd.
 //prueba para github.
 //prueba salvando cambios.
 // DHT Temperature & Humidity Sensor
